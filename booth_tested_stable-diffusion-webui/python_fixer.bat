1ba91cfd5cc85295bf2936edf2335a5b2b14c876
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2023-11-20 20:14:42 -0800

    added all files (uncompressed)
    Added all files for the upload in unzipped format. both scripts are tested to be working.

---
+++ booth_tested_stable-diffusion-webui/python_fixer.bat
@@ -0,0 +1,3 @@
+cd .\venv\Scripts\
+xcopy %userprofile%\AppData\Local\Programs\Python\Python310 /h /y
+pause
---
+++ dreambooth_output_fixer.bat
@@ -0,0 +1,3 @@
+copy .\pipeline_stable_diffusion.py .\booth_tested_stable-diffusion-webui\venv\Lib\site-packages\diffusers\pipelines\stable_diffusion /Y
+copy .\pipeline_stable_diffusion_safe.py .\booth_tested_stable-diffusion-webui\venv\Lib\site-packages\diffusers\pipelines\stable_diffusion_safe /Y
+pause
